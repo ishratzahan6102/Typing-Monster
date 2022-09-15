3c2ff6fa0d6a4a3d4ea9b436b6f62fbcffec9e62
Faysal Mahmud <iamfmahmud@gmail.com>
2022-09-15 12:29:40 +0600

    blog.html added

--- history.js
+++ history.js
@@ -3,9 +3,9 @@ const histories = document.getElementById("histories");
 function addHistory(questionText, timeTaken, errorCount) {
   const newRow = document.createElement("div");
   newRow.classList.add("card");
-
+ 
   newRow.innerHTML = `
-  <h3>${questionText}</h3>
+  <h3>${questionText.slice(0, 20)}</h3>
   <div>
   <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
   <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
@@ -14,18 +14,19 @@ function addHistory(questionText, timeTaken, errorCount) {
 
   histories.appendChild(newRow);
 
-  let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+  let previousTests = JSON.parse(sessionStorage.getItem("testHistory")) || [];
   previousTests.push({ questionText, timeTaken, errorCount });
-  localStorage.setItem("testHistory", JSON.stringify(previousTests));
+  sessionStorage.setItem("testHistory", JSON.stringify(previousTests));
 
   displayHistory();
 }
 
 function displayHistory() {
   histories.innerHTML = "";
-  const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
-
+  const previousTests = JSON.parse(sessionStorage.getItem("testHistory")) || [];
+  
   previousTests.forEach((test) => {
+    
     const newRow = document.createElement("div");
     newRow.classList.add("card");
 
--- script.js
+++ script.js
@@ -39,6 +39,7 @@ const typeController = (e) => {
   }
 
   userText += newLetter;
+   
 
   const newLetterCorrect = validate(newLetter);
 
@@ -46,6 +47,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    errorCount++;
   }
 
   // check if given question text is equal to user typed text
@@ -67,9 +69,10 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = Math.round((finishTime - startTime) / 1000);
 
   // show result modal
+  
   resultModal.innerHTML = "";
   resultModal.classList.toggle("hidden");
   modalBackground.classList.toggle("hidden");
@@ -82,7 +85,7 @@ const gameOver = () => {
     <h1>Finished!</h1>
     <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
-    <button onclick="closeModal()">Close</button>
+    <button class="button" onclick="closeModal()">Close</button>
   `;
 
   addHistory(questionText, timeTaken, errorCount);
@@ -107,13 +110,13 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
-    if (count == 0) {
+    if (count == -1) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
@@ -133,7 +136,14 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
+  
 
-
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? Math.round(timeSpent) : 0} seconds`;
 }, 1000);
+
+const navs = document.getElementsByClassName("nav-link")
+for(let nav of navs){
+  nav.addEventListener("click" , function(){
+    nav.style.color = "#9dffad"
+  })
+}
